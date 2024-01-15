bc9bb90a4bb9b4090b3031b9237a2d9accf4c035
sanemkoca <sanemkoca@gmail.com>
2024-01-15 12:30:05 +0300

    -complete -create template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,26 +7,40 @@ use App\Entity\DeveloperEntity;
 use App\Repository\DeveloperEntityRepository;
 use App\Repository\TaskEntityRepository;
 use App\Services\TaskService;
+use App\Strategy\StrategyInterface;
+use App\Strategy\StrategyOverTime;
 use App\Strategy\StrategySameLevel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(DeveloperEntityRepository $developerEntityRepository, TaskEntityRepository $taskEntityRepository): Response
+    #[Route('/default', name: 'app_default', methods: ['GET', 'POST'])]
+    public function index(
+        Request                   $request,
+        DeveloperEntityRepository $developerEntityRepository,
+        TaskEntityRepository      $taskEntityRepository): Response
     {
         $developers = $developerEntityRepository->findAll();
         $tasks = $taskEntityRepository->findAll();
 
-        $sameLevelStrategy = new StrategySameLevel($tasks, $developers);
-        $strategyContext = new AssignStrategyContext($sameLevelStrategy);
+        $selectedStrategy = $request->request->get('strategy');
 
-        $plan = $strategyContext->handle();
+        if ($selectedStrategy == 'StrategySameLevel') {
+            $sameLevelStrategy = new StrategySameLevel($tasks, $developers);
+            $strategy = $sameLevelStrategy;
+        } else{
+            $overTimeStrategy = new StrategyOverTime($tasks, $developers);
+            $strategy = $overTimeStrategy;
+        }
+        $strategyContext = new AssignStrategyContext($strategy);
+
+        $plans = $strategyContext->handle();
 
         return $this->render('default/index.html.twig', [
-            'plans' => $plan,
+            'plans' => $plans,
         ]);
     }
 }
